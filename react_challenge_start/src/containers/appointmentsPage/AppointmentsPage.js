b248a3bb6b56a458c0368c244ff83ca29d22d45d
Maxrio <lasse@rosendahl-energietechnik.de>
2022-03-13 11:59:48 +0100

    appointment page

--- react_challenge_start/src/containers/appointmentsPage/AppointmentsPage.js
+++ react_challenge_start/src/containers/appointmentsPage/AppointmentsPage.js
@@ -5,13 +5,20 @@ export const AppointmentsPage = () => {
   Define state variables for 
   appointment info
   */
+  const [title, setTitle] = React.useState("");
+  const [contact, setContact] = React.useState("");
+  const [date, setDate] = React.useState("");
+  const [time, setTime] = React.useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    setTitle(e.target.title.value);
+    setContact(e.target.contact.value);
+    setDate(e.target.date.value);
+    setTime(e.target.time.value);
   };
 
   return (
