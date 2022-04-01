ba556c59d1b516929d53967ceee014819e451f90
Maxrio <lasse@rosendahl-energietechnik.de>
2022-04-01 07:24:50 +0200

    added selector exporting

--- flashcards-starter/src/features/topics/topicsSlice.js
+++ flashcards-starter/src/features/topics/topicsSlice.js
@@ -3,14 +3,16 @@ import { createSlice } from '@reduxjs/toolkit';
 const topicsSlice = createSlice({
     name: 'topics',
     initialState: {
-        topics: []
+        topics: {}
     },
     reducers: {
         addTopic: (state, action) => {
-            state.topics.push({
+            state.topics[action.payload.id] = ({
                 ...action.payload,
                 quizIds: []
             });
         }
     }
 });
+
+export const selectTopic = (state) => state.topics;
